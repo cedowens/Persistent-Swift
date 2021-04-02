7f5d41202e2e4cd6bf1573fdb51409ba0e372e88
Cedric Owens <41086586+cedowens@users.noreply.github.com>
2021-04-01 21:46:33 -0500

    Add files via upload

---
+++ Persistent_Swift/AtomPersist/AtomPersistTests/AtomPersistTests.swift
@@ -0,0 +1,34 @@
+//
+//  AtomPersistTests.swift
+//  AtomPersistTests
+//
+//  Created by Dev on 4/1/21.
+//  Copyright © 2021 Dev. All rights reserved.
+//
+
+import XCTest
+@testable import AtomPersist
+
+class AtomPersistTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
